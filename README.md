a4e1acd08354cdaaf125dd72b336d3b604833f49
jhons1206 <webdeveloper84@gmail.com>
2023-04-19 08:34:16 -0500

    Proyecto: Cotizador de Préstamos en Vue.js - Cap. 1-9

---
+++ README.md
@@ -0,0 +1,3 @@
+# JavaScript Moderno Guía Definitiva Construye +20 Proyectos
+## Cotizador de Préstamos (Desarrollado con Vue 3, Vite)
+###### Instructor: Código con Juan
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, reactive } from 'vue';
+import Header from './components/Header.vue';
+
+const cantidad = ref(0);
+
+const state = reactive({
+    cantidad: 0,
+});
+
+function handleChange(e) {
+    cantidad.value = e.target.value;
+}
+
+console.log(cantidad.value);
+
+console.log(state.cantidad);
+</script>
+
+<template>
+    <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+        <Header />
+
+        <div class="my-5">
+            <input type="range" class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600" @input="handleChange" />
+        </div>
+    </div>
+</template>
