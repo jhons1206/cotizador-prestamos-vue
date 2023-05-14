ae007c75e91c88158b36b761c64c69a7daf159b7
Jhon Sandoval <webdeveloper84@gmail.com>
2023-05-13 20:42:34 -0500

    Proyecto: Cotizador de Préstamos en Vue.js - TERMINADO

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,55 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 import Header from './components/Header.vue';
+import Button from './components/Button.vue';
+import { calcularTotalPagar } from './helpers';
 
 const cantidad = ref(10000);
-const MIN = 2000;
+const meses = ref(6);
+const total = ref(0);
+const MIN = 0;
 const MAX = 20000;
 const STEP = 100;
 
-const formatearDinero = computed(() => {
+const formatearDinero = (valor) => {
     const formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD',
     });
+    return formatter.format(valor);
+};
 
-    return formatter.format(cantidad.value);
+watch(
+    [cantidad, meses],
+    () => {
+        total.value = calcularTotalPagar(cantidad.value, meses.value);
+    },
+    { immediate: true }
+);
+
+const pagoMensual = computed(() => {
+    return total.value / meses.value;
 });
 
 const handleChangeDecremento = () => {
     const valor = cantidad.value - STEP;
-    if(valor < MIN) {
+    if (valor < MIN) {
         alert('Cantidad no válida');
         return;
     }
 
     cantidad.value = valor;
-}
+};
 
+const handleChangeIncremento = () => {
+    const valor = cantidad.value + STEP;
+    if (valor > MAX) {
+        alert('Cantidad no válida');
+        return;
+    }
+
+    cantidad.value = valor;
+};
 </script>
 
 <template>
@@ -33,16 +57,33 @@ const handleChangeDecremento = () => {
         <Header />
 
         <div class="flex justify-between mt-10">
-            <button class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 text-white text-2xl" @click="handleChangeDecremento">-</button>
-
-            <button class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 text-white text-2xl">+</button>
+            <Button :operador="'-'" @fn="handleChangeDecremento" />
+            <Button :operador="'+'" @fn="handleChangeIncremento" />
         </div>
 
         <div class="my-5">
             <input type="range" class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600" :min="MIN" :max="MAX" :step="STEP" v-model.number="cantidad" />
 
             <!-- {{state.cantidad}} -->
-            <p class="text-center my-10 text-5xl font-extrabold text-indigo-600">{{formatearDinero}}</p>
+            <p class="text-center my-10 text-5xl font-extrabold text-indigo-600">{{ formatearDinero(cantidad) }}</p>
+
+            <h2 class="text-2xl font-extrabold text-gray-500 text-center">Elige un <span class="text-indigo-600">Plazo</span> a pagar</h2>
+
+            <select class="w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500 mt-5" :value="meses" v-model.number="meses">
+                <option value="6">6 Meses</option>
+                <option value="12">12 Meses</option>
+                <option value="24">24 Meses</option>
+            </select>
         </div>
+
+        <div v-if="total > 0" class="my-5 space-y-3 bg-gray-50 p-5">
+            <h2 class="text-2xl font-extrabold text-gray-500 text-center">Resumen <span class="text-indigo-600">de pagos</span></h2>
+
+            <p class="text xl text-gray-500 text-center font-bold">{{ meses }} Meses</p>
+            <p class="text xl text-gray-500 text-center font-bold">Total a pagar: {{ formatearDinero(total) }}</p>
+            <p class="text xl text-gray-500 text-center font-bold">Mensuales: {{ formatearDinero(pagoMensual) }}</p>
+        </div>
+
+        <p v-else class="text xl text-pink-700 text-center font-bold">Añade una cantidad y un plazo a pagar</p>
     </div>
 </template>
